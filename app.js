99e49d3aae07760a9cfead4f87a32ae5ff7d382c
Adrian Tam <tam.ad@northeastern.edu>
2023-12-04 03:28:27 -0500

    a6

--- app.js
+++ app.js
@@ -2,15 +2,37 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
-// import session from "express-session";
+import session from "express-session";
 
 const app = express();
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
 
-app.use(cors());
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
