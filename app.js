72456722d1f30f2f02e2d3b123022b42790248cb
Adrian Tam <tam.ad@northeastern.edu>
2023-12-04 11:47:21 -0500

    fixed error

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ import Lab5 from "./lab5.js";
 import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
 import session from "express-session";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors({
